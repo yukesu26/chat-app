985cac6a703267e6c7a3534475b0d57ce002f722
yukesu26 <yusuke.562139@gmail.com>
2020-12-15 21:33:16 +0900

    room_usersテーブルの作成

--- db/migrate/20201215100713_create_rooms.rb
+++ db/migrate/20201215100713_create_rooms.rb
@@ -1,8 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-     t.string :name, null: false
-     t.timestamps
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
     end
   end
 end
