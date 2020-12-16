e2b66423c3efc0bba87820720d7b1e63928750a5
yukesu26 <yusuke.562139@gmail.com>
2020-12-16 16:28:12 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,12 @@
 class RoomsController < ApplicationController
   
+  def new
+    @room = Room.new
+  end
+  
   def index
   end
   
-  
-
   def create
     @room = Room.new(room_params)
     if @room.save
