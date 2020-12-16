c6d7c3582b621b705f2ee774ad24394ad410f73f
yukesu26 <yusuke.562139@gmail.com>
2020-12-16 17:41:34 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,12 +1,13 @@
 class RoomsController < ApplicationController
-  
+
+  def index
+
+  end
+
   def new
     @room = Room.new
   end
-  
-  def index
-  end
-  
+
   def create
     @room = Room.new(room_params)
     if @room.save
@@ -16,9 +17,15 @@ class RoomsController < ApplicationController
     end
   end
 
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
